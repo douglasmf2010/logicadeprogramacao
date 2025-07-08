973284854d47a066da04a33ce2af904a60cb315d
Douglas Mattozo Ferenandes <douglasmattozofernandes188@gmail.com>
2025-07-08 17:14:19 -0300

    Add files via upload

---
+++ projeto 28/livro.js
@@ -0,0 +1,8 @@
+const capa = document.getElementById("capa")
+const pag1 = document.getElementById("pag1")
+const pag2 = document.getElementById("pag2")
+
+document.getElementById('pxmpergunta').onclick = function () {
+    pag1.style.display = 'none';
+    pag2.style.display = 'inline-block';
+}
